9754aa9a6345dbe1ccf44eab91693e8719bcc784
Hubert Wiśniewski <hubert_w@autograf.pl>
2019-10-18 19:34:09 +0200

    Updates

---
+++ precache-manifest.9b151b9219e03f5a890075a8c3022d56.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490b59df7083ec05220111824182953a",
+    "url": "/agenctwo/index.html"
+  },
+  {
+    "revision": "345266e9cc5b298c71ed",
+    "url": "/agenctwo/static/css/2.2e2f19ad.chunk.css"
+  },
+  {
+    "revision": "79a2ddbe8f0124fbed7d",
+    "url": "/agenctwo/static/css/main.9b6d7447.chunk.css"
+  },
+  {
+    "revision": "345266e9cc5b298c71ed",
+    "url": "/agenctwo/static/js/2.4508ab15.chunk.js"
+  },
+  {
+    "revision": "79a2ddbe8f0124fbed7d",
+    "url": "/agenctwo/static/js/main.eaca6334.chunk.js"
+  },
+  {
+    "revision": "2414caa9ac8f2f80fe3d",
+    "url": "/agenctwo/static/js/runtime-main.93a49d8a.js"
+  },
+  {
+    "revision": "1f437fbea89469e3e5186978709d81f4",
+    "url": "/agenctwo/static/media/baloon.1f437fbe.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/agenctwo/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/agenctwo/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/agenctwo/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/agenctwo/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/agenctwo/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/agenctwo/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/agenctwo/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/agenctwo/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/agenctwo/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/agenctwo/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/agenctwo/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "f55e56710b0b2083d4f3d5f7358c75f6",
+    "url": "/agenctwo/static/media/logo.f55e5671.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/agenctwo/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/agenctwo/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/agenctwo/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/agenctwo/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/agenctwo/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "938862d676dc8a41b47c8c59eb8f2a69",
+    "url": "/agenctwo/static/media/tlo.938862d6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d565424b2fe807d0e5d711b93ac5fcb1.js"
+  "/agenctwo/precache-manifest.9b151b9219e03f5a890075a8c3022d56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/agenctwo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
